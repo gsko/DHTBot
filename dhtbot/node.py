ddd211ead83a20372f02d5dfd36dd5e621226302
Greg Skoczek <gsk067@illinois.edu>
2012-05-14 18:41:49 -0500

    added xml_rpc wrapping around the krpc_responder node

--- dhtbot/node.py
+++ dhtbot/node.py
@@ -9,18 +9,23 @@ from dhtbot.protocols.krpc_responder import KRPC_Responder
 from dhtbot.xml_rpc.server_service import KRPC_Responder_Server
 
 udp_port = 1800
-rand_id = 317981748868523100801836251505537304032928861248L
+node_id = 317981748868523100801836251505537304032928861248L
 
 # Set up the root application
 application = service.Application("DHT Daemon")
 
+# The protocol that will drive the heart of this
+# application
+node_proto = KRPC_Responder(node_id=rand_id)
+
 # Set up the XML RPC wrapper
-xml_rpc = KRPC_Responder_Server()
-node_xml_rpc = internet.TCPServer(8888, server.site(xml_rpc))
+xml_rpc = KRPC_Responder_Server(node_proto)
+node_xml_rpc = service.MultiService()
+xml_rpc_tcp = internet.TCPServer(8888, server.Site(xml_rpc))
+xml_rpc_tcp.setServiceParent(node_xml_rpc)
 node_xml_rpc.setServiceParent(application)
 
 # Set up the actual node that will be receiving, responding to,
 # and receiving queries
-node_proto = KRPC_Responder(node_id=rand_id)
 node_udp = internet.UDPServer(udp_port, node_proto)
-node_service.setServiceParent(node_xml_rpc)
+node_udp.setServiceParent(node_xml_rpc)
