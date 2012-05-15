cb6397b2f40b83c753a6432ef96475b6167cddbd
Greg Skoczek <gsk067@illinois.edu>
2012-05-14 19:34:13 -0500

    added a find_node example

---
+++ examples/xml_rpc/find_node.py
@@ -0,0 +1,10 @@
+from dhtbot.xml_rpc.client import KRPC_Responder_Client
+
+k = KRPC_Responder_Client("http://localhost:8888")
+host = ("67.18.187.143", 1337)
+infohash = 0x56CB72C464912215CB5BC1ADEB77E5A50856D887
+
+response = k.find_node(host, infohash)
+nodes = response.nodes
+for node in nodes:
+    print node.node_id, node.address
