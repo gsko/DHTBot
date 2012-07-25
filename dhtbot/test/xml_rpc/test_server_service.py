47eadbf459b50e11ab54256b26db10cbb03698dd
Greg Skoczek <gsk067@illinois.edu>
2012-07-25 17:13:58 -0500

    Implemented KRPC_Sender_Server test case that ensures that the proper arguments are being passed to the underlying KRPC_Sender node protocol. This test case is currently passing. Stubbed out the KRPC_Responder_Server test case in a similar fashion.

--- dhtbot/test/xml_rpc/test_server_service.py
+++ dhtbot/test/xml_rpc/test_server_service.py
@@ -1,6 +1,8 @@
 from twisted.trial import unittest
+from twisted.internet import defer
 
-
+from dhtbot.krpc_types import Query
+from dhtbot.xml_rpc.pickle_utils import pickle_to_str, unpickle_from_str
 from dhtbot.xml_rpc.server_service import (KRPC_Sender_Server,
         KRPC_Responder_Server, KRPC_Iterator_Server)
 
@@ -21,12 +23,18 @@ class Hollow_KRPC_Iterator(object):
     >>> 
 
     """
+    def __init__(self, *args):
+        assert 0 <= len(args) <= 2
+        self.args = ()
+        self.called = None
+
     ## KRPC_Sender
     #
 
     def sendQuery(self, query, address, timeout=None):
         self.args = (query, address, timeout)
         self.called = self.sendQuery
+        return defer.Deferred()
 
     #
     #
@@ -37,18 +45,22 @@ class Hollow_KRPC_Iterator(object):
     def ping(self, address, timeout=None):
         self.args = (address, timeout)
         self.called = self.ping
+        return defer.Deferred()
 
     def find_node(self, address, node_id, timeout=None):
         self.args = (address, node_id, timeout)
         self.called = self.find_node
+        return defer.Deferred()
 
     def get_peers(self, address, target_id, timeout=None):
         self.args = (address, target_id, timeout)
         self.called = self.get_peers
+        return defer.Deferred()
 
     def announce_peer(self, address, target_id, token, port, timeout=None):
         self.args = (address, target_id, token, port, timeout)
         self.called = self.announce_peer
+        return defer.Deferred()
 
     #
     #
@@ -59,10 +71,58 @@ class Hollow_KRPC_Iterator(object):
     def find_iterate(self, target_id, nodes=None, timeout=None):
         self.args = (target_id, nodes, timeout)
         self.called = self.find_iterate
+        return defer.Deferred()
 
     def get_iterate(self, target_id, nodes=None, timeout=None):
         self.args = (target_id, nodes, timeout)
         self.called = self.get_iterate
+        return defer.Deferred()
 
-class KRPC_Sender_Server_TestCase(unittest.TestCase):
-    pass
+class ServerTestCaseBase(object):
+    test_address = ("127.0.0.2", 22)
+
+    def setUp(self):
+        self.node = Hollow_KRPC_Iterator()
+
+class KRPC_Sender_Server_TestCase(unittest.TestCase, ServerTestCaseBase):
+    """
+    This test case ensures that the KRPC_Sender_Server
+    properly passes all of its arguments to the
+    underlying node protocol
+
+    """
+    def setUp(self):
+        ServerTestCaseBase.setUp(self)
+        self.kserver = KRPC_Sender_Server(self.node)
+        q = Query()
+        q.rpctype = "ping"
+        q._from = 42
+        self.test_query = q
+
+    def test_sendQuery_args(self):
+        self.kserver.xmlrpc_sendQuery(pickle_to_str(self.test_query),
+                list(self.test_address), 55)
+        expected_args = (self.test_query, self.test_address, 55)
+        self.assertEquals(expected_args, self.node.args)
+        self.assertEquals(self.node.called, self.node.sendQuery)
+
+
+class KRPC_Responder_Server_TestCase(unittest.TestCase, ServerTestCaseBase):
+    """Same as KRPC_Sender_Server_TestCase, but for KRPC_Responder Server"""
+
+    def setUp(self):
+        ServerTestCaseBase.setUp(self)
+        self.kserver = KRPC_Responder_Server(self.node)
+
+    def test_ping_args(self):
+        pass
+
+    def test_find_node_args(self):
+        pass
+
+    def test_get_peers_args(self):
+        pass
+
+    def test_announce_peer_args(self):
+        pass
+
